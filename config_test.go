a65dea56b62b84a266ff68ffee1f38460123ddf1
Keko <luismesas@gmail.com>
2014-01-27 20:50:12 +0100

    added example por package

--- config_test.go
+++ config_test.go
@@ -5,32 +5,59 @@ import (
 	"github.com/bbva-innotech/go-json-config"
 )
 
-func ExampleString() {
+func Example() {
 
 	// content of config.json file
 	// {
-	// 	"key-string" : "value",
+	// 	"key-string" : "value-string",
 	// 	"key-array" : [
-	// 		"value1",
-	// 		"value2",
-	// 		"value3"
+	// 		"value-array-1",
+	// 		"value-array-2",
+	// 		"value-array-3"
 	// 	]
 	// }
 
 	value := config.String("key-string")
 	fmt.Println(value)
-	// Output: value1
+
+	values := config.StringArray("key-array")
+	for _, value := range values {
+		fmt.Println(value)
+	}
+	// Output:
+	// value-string
+	// value-array-1
+	// value-array-2
+	// value-array-3
+}
+
+func ExampleString() {
+
+	// content of config.json file
+	// {
+	// 	"key-string" : "value-string",
+	// 	"key-array" : [
+	// 		"value-array-1",
+	// 		"value-array-2",
+	// 		"value-array-3"
+	// 	]
+	// }
+
+	value := config.String("key-string")
+	fmt.Println(value)
+	// Output:
+	// value-string
 }
 
 func ExampleStringArray() {
 
 	// content of config.json file
 	// {
-	// 	"key-string" : "value",
+	// 	"key-string" : "value-string",
 	// 	"key-array" : [
-	// 		"value1",
-	// 		"value2",
-	// 		"value3"
+	// 		"value-array-1",
+	// 		"value-array-2",
+	// 		"value-array-3"
 	// 	]
 	// }
 
@@ -39,7 +66,7 @@ func ExampleStringArray() {
 		fmt.Println(value)
 	}
 	// Output:
-	// value1
-	// value2
-	// value3
+	// value-array-1
+	// value-array-2
+	// value-array-3
 }
